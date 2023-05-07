d5513f84d8a8ec18cc638e4d63ba97c731926214
Azizjon450 <qodirovazizjon.natura@gmail.com>
2023-05-07 16:44:39 +0500

    create function get from Firebase

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -6,30 +6,58 @@ import '../models/product.dart';
 import '../providers/products.dart';
 import 'product_item.dart';
 
-class ProductsGrid extends StatelessWidget {
+class ProductsGrid extends StatefulWidget {
   final bool showOnlyFavorites;
   const ProductsGrid(this.showOnlyFavorites, {super.key});
+
+  @override
+  State<ProductsGrid> createState() => _ProductsGridState();
+}
+
+class _ProductsGridState extends State<ProductsGrid> {
+  var _init = true;
+
+  @override
+  void initState() {
+    //   Future.delayed(Duration.zero).then((value) {
+    //   Provider.of<Products>(context, listen: false).getProductsFromFirebase();
+    // });             
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_init) {
+      Provider.of<Products>(context, listen: false).getProductsFromFirebase();
+    }
+    _init = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     final products =
-        showOnlyFavorites ? productsData.favorites : productsData.list;
-    return GridView.builder(
-      physics: const BouncingScrollPhysics(),
-      padding: const EdgeInsets.all(20),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-          childAspectRatio: 3 / 2,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20),
-      itemCount: products.length,
-      itemBuilder: ((context, index) {
-        return ChangeNotifierProvider<Product>.value(
-          value: products[index],
-          child: const ProductItem(),
-        );
-      }),
-    );
+        widget.showOnlyFavorites ? productsData.favorites : productsData.list;
+    return products.isNotEmpty
+        ? GridView.builder(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(20),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                childAspectRatio: 3 / 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20),
+            itemCount: products.length,
+            itemBuilder: ((context, index) {
+              return ChangeNotifierProvider<Product>.value(
+                value: products[index],
+                child: const ProductItem(),
+              );
+            }),
+          )
+        : Center(
+            child: Text("Mahsulotlar mavjud emas!"),
+          );
   }
 }
